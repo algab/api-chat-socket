f5005af84d6b86d0b95a364bd41a36be0bf9e250
Álvaro Oliveira <alvarogab6@gmail.com>
2019-12-12 12:07:23 -0300

    add route conversation

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
+const conversation = require('./modules/conversation');
 const user = require('./modules/user');
 
 module.exports = (app) => {
+    app.use('/conversations', conversation);
     app.use('/users', user);
 };
