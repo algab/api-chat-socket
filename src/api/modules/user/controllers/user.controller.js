1cabfdfae786cdeb50ab6f91a11a1dc548c9e1ff
Álvaro Oliveira <alvarogab6@gmail.com>
2019-12-18 11:54:15 -0300

    fix modules user

--- src/api/modules/user/controllers/user.controller.js
+++ src/api/modules/user/controllers/user.controller.js
@@ -8,6 +8,7 @@ class UserController {
         this.save = this.save.bind(this);
         this.list = this.list.bind(this);
         this.search = this.search.bind(this);
+        this.login = this.login.bind(this);
         this.update = this.update.bind(this);
     }
 
@@ -17,8 +18,8 @@ class UserController {
             const result = await this.User.find({ email: data.email }).countDocuments();
             if (!result) {
                 data.avatar_url = `https://www.gravatar.com/avatar/${hasha(data.email, { algorithm: 'md5' })}?d=retro`;
-                await new this.User(data).save();
-                res.status(201).json({ Message: 'User save successful' }).end();
+                const newUser = await new this.User(data).save();
+                res.status(201).json(newUser).end();
             } else {
                 res.status(409).json({ Message: 'Email conflict' }).end();
             }
@@ -40,6 +41,24 @@ class UserController {
         try {
             const data = await this.User.findById(params.id);
             res.status(200).json(data).end();
+        } catch (error) {
+            res.status(500).json({ Message: 'Server Error' }).end();
+        }
+    }
+
+    async login({ body }, res) {
+        try {
+            if (body.email && body.password) {
+                const { email, password } = body;
+                const data = await this.User.findOne({ email, password });
+                if (data) {
+                    res.status(200).json(data).end();
+                } else {
+                    res.status(404).json({ Message: 'User not found' }).end();
+                }
+            } else {
+                res.status(400).json({ Message: 'E-mail and password is required ' }).end();
+            }
         } catch (error) {
             res.status(500).json({ Message: 'Server Error' }).end();
         }
--- src/api/modules/user/index.js
+++ src/api/modules/user/index.js
@@ -6,6 +6,7 @@ const validate = require('../../middlewares/validate.middleware');
 router.post('/', validate(model), user.save);
 router.get('/', user.list);
 router.get('/:id', user.search);
+router.put('/login', user.login);
 router.put('/:id', validate(model), user.update);
 
 module.exports = router;
